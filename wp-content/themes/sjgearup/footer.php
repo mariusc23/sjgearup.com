ad901d510577a05840ec2fa6ed975dbce45599eb
Marius Craciunoiu <marius@Marius-Craciunoius-MacBook-Pro.local>
2010-11-01 20:46:01 -0700

    Webkit Fix and Analytics

--- wp-content/themes/sjgearup/footer.php
+++ wp-content/themes/sjgearup/footer.php
@@ -131,5 +131,18 @@ $(document).ready(function() {
 	auto_scroll_carousel(true, true);
 });
 </script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-19454061-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
